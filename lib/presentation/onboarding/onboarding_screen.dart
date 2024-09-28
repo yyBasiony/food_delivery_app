97971156ef11298038b0e36c63523f839d65eab3
yasmine <iayasmine34@gmail.com>
2024-09-29 02:20:39 +0300

    add responsive to onboarding screen

--- lib/presentation/onboarding/onboarding_screen.dart
+++ lib/presentation/onboarding/onboarding_screen.dart
@@ -13,7 +13,6 @@ class OnboardingScreen extends StatefulWidget {
 class _OnboardingScreenState extends State<OnboardingScreen> {
   int pageIndex = 0;
   late PageController pageController;
-
   @override
   void dispose() => {pageController.dispose(), super.dispose()};
 
@@ -22,69 +21,110 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
 
   void _goNextPage() => pageIndex == AppConstants.onboardingData.length - 1
       ? Navigator.pushReplacementNamed(context, Routes.welcomeScreen)
-      : pageController.nextPage(duration: const Duration(milliseconds: 500), curve: Curves.easeIn);
-
+      : pageController.nextPage(
+          duration: const Duration(milliseconds: 300), curve: Curves.easeIn);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.primaryColor,
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                style: const ButtonStyle(alignment: AlignmentDirectional.centerEnd),
-                onPressed: () => Navigator.pushReplacementNamed(context, Routes.welcomeScreen),
-                child: Text('Skip', style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Colors.white)),
-              ),
-            ),
-            Expanded(
-              child: PageView.builder(
-                controller: pageController,
-                itemCount: AppConstants.onboardingData.length,
-                onPageChanged: (index) => setState(() => pageIndex = index),
-                itemBuilder: (_, index) {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Spacer(),
-                      Image.asset(AppConstants.onboardingData[index].image, width: 190, height: 160),
-                      Text(AppConstants.onboardingData[index].title, style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Colors.white)),
-                      Text(
-                        textAlign: TextAlign.center,
-                        AppConstants.onboardingData[index].description,
-                        style: Theme.of(context).textTheme.bodySmall?.copyWith(color: Colors.white),
-                      ),
-                      const Spacer(),
-                    ],
-                  );
-                },
-              ),
-            ),
-            const SizedBox(height: 50),
-            ElevatedButton(
-              onPressed: _goNextPage,
-              style: const ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.white)),
-              child: Text("Continue", style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: AppColors.primaryColor)),
-            ),
-            const SizedBox(height: 50),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(
-                AppConstants.onboardingData.length,
-                (index) => AnimatedContainer(
-                  width: 14,
-                  height: 14,
-                  duration: const Duration(milliseconds: 500),
-                  margin: const EdgeInsets.symmetric(horizontal: 4),
-                  decoration: BoxDecoration(shape: BoxShape.circle, color: pageIndex == index ? Colors.white : AppColors.inactive),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    style: const ButtonStyle(
+                        alignment: AlignmentDirectional.centerEnd),
+                    onPressed: () => Navigator.pushReplacementNamed(
+                        context, Routes.welcomeScreen),
+                    child: Text('Skip',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyMedium
+                            ?.copyWith(color: Colors.white)),
+                  ),
                 ),
-              ),
-            ),
-          ],
+                Expanded(
+                  child: PageView.builder(
+                    controller: pageController,
+                    itemCount: AppConstants.onboardingData.length,
+                    onPageChanged: (index) => setState(() => pageIndex = index),
+                    itemBuilder: (_, index) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            AppConstants.onboardingData[index].image,
+                            width: 190,
+                            height: 160,
+                          ),
+                          Text(
+                            AppConstants.onboardingData[index].title,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium
+                                ?.copyWith(color: Colors.white),
+                          ),
+                          Text(
+                            textAlign: TextAlign.center,
+                            AppConstants.onboardingData[index].description,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodySmall
+                                ?.copyWith(color: Colors.white),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: ElevatedButton(
+                        onPressed: _goNextPage,
+                        style: const ButtonStyle(
+                            backgroundColor:
+                                WidgetStatePropertyAll(Colors.white)),
+                        child: Text(
+                          "Continue",
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium
+                              ?.copyWith(color: AppColors.primaryColor),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(32.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: List.generate(
+                          AppConstants.onboardingData.length,
+                          (index) => AnimatedContainer(
+                            width: 14,
+                            height: 14,
+                            duration: const Duration(milliseconds: 300),
+                            margin: const EdgeInsets.symmetric(horizontal: 4),
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: pageIndex == index
+                                    ? Colors.white
+                                    : AppColors.inactive),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
